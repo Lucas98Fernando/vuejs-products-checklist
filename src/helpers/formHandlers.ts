319095ef822fa1f9078458d91c7806f9ab024c24
Lucas Fernando <lucas98fernando@gmail.com>
2021-12-05 19:44:06 -0300

    Refatorando o código e corrigindo problemas de tipagem

--- src/helpers/formHandlers.ts
+++ src/helpers/formHandlers.ts
@@ -1,13 +1,17 @@
-export const validate = ({ context }: any): boolean => {
+interface IFormHandler {
+  context: unknown;
+}
+
+export const validate = ({ context }: IFormHandler): boolean => {
   return (context as Vue & { validate: () => boolean }).validate();
 };
 
-export const resetValidation = ({ context }: any): boolean => {
+export const resetValidation = ({ context }: IFormHandler): boolean => {
   return (
     context as Vue & { resetValidation: () => boolean }
   ).resetValidation();
 };
 
-export const reset = ({ context }: any): boolean => {
+export const reset = ({ context }: IFormHandler): boolean => {
   return (context as Vue & { reset: () => boolean }).reset();
 };
--- src/services/interceptors.ts
+++ src/services/interceptors.ts
@@ -3,49 +3,53 @@ import { removeAllStorageData } from "@/helpers/storage";
 
 const interceptors = (vm: any) =>
   api.interceptors.response.use(
-    (response) => {
-      return response;
-    },
+    (response) => response,
     (error) => {
-      if (error.response.status === 400) {
-        vm.$root.$emit("show-base-dialog", {
-          srcImg: require("@/assets/images/illustrations/waiting.png"),
-          titleMessage: "Atenção!",
-          bodyMessage: error.response.data.message,
-          colorText: "red",
-        });
-      } else if (error.response.status === 401) {
-        removeAllStorageData();
-        vm.$router.replace("/auth/login");
-        vm.$root.$emit("show-base-dialog", {
-          srcImg: require("@/assets/images/illustrations/comeback.png"),
-          titleMessage: "Credenciais inválidas",
-          bodyMessage: error.response.data.message,
-          colorText: "red",
-        });
-      } else if (error.response.status === 403) {
-        vm.$root.$emit("show-base-dialog", {
-          srcImg: require("@/assets/images/illustrations/waiting.png"),
-          titleMessage: "Não autorizado!",
-          bodyMessage: error.response.data.error,
-          colorText: "red",
-        });
-      } else if (error.response.status === 404) {
-        vm.$root.$emit("show-base-dialog", {
-          srcImg: require("@/assets/images/illustrations/page-not-found.png"),
-          titleMessage: "O recurso solicitado não foi encontrado!",
-          bodyMessage: error.response.data.error,
-          colorText: "red",
-        });
-      } else {
-        removeAllStorageData();
-        vm.$router.replace("/auth/login");
-        vm.$root.$emit("show-base-dialog", {
-          srcImg: require("@/assets/images/illustrations/waiting.png"),
-          titleMessage: "Ocorreu um erro",
-          bodyMessage: error.response.data.error,
-          colorText: "red",
-        });
+      switch (error.response.status) {
+        case 400:
+          vm.$root.$emit("show-base-dialog", {
+            srcImg: require("@/assets/images/illustrations/waiting.png"),
+            titleMessage: "Atenção!",
+            bodyMessage: error.response.data.message,
+            colorText: "red",
+          });
+          break;
+        case 401:
+          removeAllStorageData();
+          vm.$router.replace("/auth/login");
+          vm.$root.$emit("show-base-dialog", {
+            srcImg: require("@/assets/images/illustrations/comeback.png"),
+            titleMessage: "Credenciais inválidas",
+            bodyMessage: error.response.data.message,
+            colorText: "red",
+          });
+          break;
+        case 403:
+          vm.$root.$emit("show-base-dialog", {
+            srcImg: require("@/assets/images/illustrations/waiting.png"),
+            titleMessage: "Não autorizado!",
+            bodyMessage: error.response.data.error,
+            colorText: "red",
+          });
+          break;
+        case 404:
+          vm.$root.$emit("show-base-dialog", {
+            srcImg: require("@/assets/images/illustrations/page-not-found.png"),
+            titleMessage: "O recurso solicitado não foi encontrado!",
+            bodyMessage: error.response.data.error,
+            colorText: "red",
+          });
+          break;
+        default:
+          removeAllStorageData();
+          vm.$router.replace("/auth/login");
+          vm.$root.$emit("show-base-dialog", {
+            srcImg: require("@/assets/images/illustrations/waiting.png"),
+            titleMessage: "Ocorreu um erro",
+            bodyMessage: error.response.data.error,
+            colorText: "red",
+          });
+          break;
       }
       return Promise.reject(error);
     }
